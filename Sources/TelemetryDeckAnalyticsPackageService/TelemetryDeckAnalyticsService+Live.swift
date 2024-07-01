8edf104d5ee6721f370f41fb64e8c0bc6da8cb56
Joseph Roque <contact@josephroque.ca>
2024-07-01 01:06:27 -0600

    fix: use correct TelemetryDeck.Config

--- Sources/TelemetryDeckAnalyticsPackageService/TelemetryDeckAnalyticsService+Live.swift
+++ Sources/TelemetryDeckAnalyticsPackageService/TelemetryDeckAnalyticsService+Live.swift
@@ -24,7 +24,7 @@ extension AnalyticsService: DependencyKey {
 				bundle.object(forInfoDictionaryKey: "TELEMETRY_DECK_API_KEY") as? String ?? ""
 			}()
 
-			let configuration = TelemetryDeck.Configuration(appID: telemetryDeckApiKey)
+			let configuration = TelemetryDeck.Config(appID: telemetryDeckApiKey)
 			if telemetryDeckApiKey.isEmpty {
 				print("Analytics disabled")
 				configuration.analyticsDisabled = true
