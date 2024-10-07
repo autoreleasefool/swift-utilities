23f768664d2a77acd0d104652692a0623d7111ae
Joseph Roque <contact@josephroque.ca>
2024-10-06 23:00:16 -0700

    feat: FileCoordinatorService

---
+++ Sources/FileManagerPackageService/FileCoordinatorService+Live.swift
@@ -0,0 +1,93 @@
+import Dependencies
+import FileManagerPackageServiceInterface
+import Foundation
+
+private actor FileCoordinator {
+	let coordinator = NSFileCoordinator()
+
+	func read(
+		itemAt url: URL,
+		options: NSFileCoordinator.ReadingOptions,
+		accessor: @Sendable (URL) throws -> Void
+	) throws {
+		var coordinatorError: NSError?
+		var readError: Error?
+
+		coordinator.coordinate(readingItemAt: url, options: options, error: &coordinatorError) { url in
+			do {
+				try accessor(url)
+			} catch {
+				readError = error
+			}
+		}
+
+		if let readError {
+			throw readError
+		}
+
+		if let coordinatorError {
+			throw coordinatorError
+		}
+	}
+
+	func write(
+		itemAt url: URL,
+		options: NSFileCoordinator.WritingOptions,
+		accessor: @Sendable (URL) throws -> Void
+	) throws {
+		var coordinatorError: NSError?
+		var readError: Error?
+
+		coordinator.coordinate(writingItemAt: url, options: options, error: &coordinatorError) { url in
+			do {
+				try accessor(url)
+			} catch {
+				readError = error
+			}
+		}
+
+		if let readError {
+			throw readError
+		}
+
+		if let coordinatorError {
+			throw coordinatorError
+		}
+	}
+}
+
+extension FileCoordinatorService: DependencyKey {
+	public static var liveValue: Self {
+		let coordinators = LockIsolated<[FileCoordinatorID: FileCoordinator]>([:])
+
+		return Self(
+			createCoordinator: {
+				@Dependency(\.uuid) var uuid
+				let id = uuid()
+				coordinators.withValue { $0[id] = FileCoordinator() }
+				return id
+			},
+			discardCoordinator: { id in
+				coordinators.withValue { $0[id] = nil }
+			},
+			read: { url, id, options, accessor in
+				guard let coordinator = coordinators.value[id] else {
+					throw FileCoordinatorServiceError.coordinatorDoesNotExist
+				}
+
+				try await coordinator.read(itemAt: url, options: options, accessor: accessor)
+			},
+			write: { url, id, options, accessor in
+				guard let coordinator = coordinators.value[id] else {
+					throw FileCoordinatorServiceError.coordinatorDoesNotExist
+				}
+
+				try await coordinator.write(itemAt: url, options: options, accessor: accessor)
+			}
+		)
+	}
+}
+
+public enum FileCoordinatorServiceError: Error {
+	case coordinatorDoesNotExist
+}
---
+++ Sources/FileManagerPackageServiceInterface/FileCoordinatorService+Interface.swift
@@ -0,0 +1,36 @@
+import Dependencies
+import DependenciesMacros
+import Foundation
+
+public typealias FileCoordinatorID = UUID
+
+@DependencyClient
+public struct FileCoordinatorService: Sendable {
+	public var createCoordinator: @Sendable () -> FileCoordinatorID = {
+		unimplemented("\(Self.self).createCoordinator", placeholder: UUID())
+	}
+	public var discardCoordinator: @Sendable (FileCoordinatorID) -> Void
+	public var read: @Sendable (
+		_ itemAt: URL,
+		_ withCoordinator: FileCoordinatorID,
+		_ options: NSFileCoordinator.ReadingOptions,
+		_ accessor: @Sendable (URL) throws -> Void
+	) async throws -> Void
+	public var write: @Sendable (
+		_ itemAt: URL,
+		_ withCoordinator: FileCoordinatorID,
+		_ options: NSFileCoordinator.WritingOptions,
+		_ accessor: @Sendable (URL) throws -> Void
+	) async throws -> Void
+}
+
+extension FileCoordinatorService: TestDependencyKey {
+	public static var testValue: Self { Self() }
+}
+
+extension DependencyValues {
+	public var fileCoordinator: FileCoordinatorService {
+		get { self[FileCoordinatorService.self] }
+		set { self[FileCoordinatorService.self] = newValue }
+	}
+}
