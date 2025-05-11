ced4d2b4c87e355ac8c5990c82ab1da93136dd1e
Joseph Roque <contact@josephroque.ca>
2025-05-11 12:36:06 -0700

    chore(ios): bump GRDB to 7.5.0

--- Package.swift
+++ Package.swift
@@ -52,7 +52,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.4"),
 		.package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.50.1"),
-		.package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
+		.package(url: "https://github.com/groue/GRDB.swift.git", from: "7.5.0"),
 		.package(url: "https://github.com/pointfreeco/swift-concurrency-extras.git", from: "1.3.1"),
 		.package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.9.2"),
 		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.3"),
